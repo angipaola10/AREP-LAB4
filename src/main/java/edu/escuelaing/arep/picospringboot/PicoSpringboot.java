6a035342211fdc10d360cc4359a7d24173485196
angipaola10 <angi.jimenez@mail.escuelaing.edu.co>
2021-03-05 22:30:12 -0500

    fix deploy

--- src/main/java/edu/escuelaing/arep/picospringboot/PicoSpringboot.java
+++ src/main/java/edu/escuelaing/arep/picospringboot/PicoSpringboot.java
@@ -1,15 +1,9 @@
 package edu.escuelaing.arep.picospringboot;
 
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Class thats represents the entry of a small implementation of Spring Boot framework.
  */
 public class PicoSpringboot {
-
-    private Map<String, Method> beans = new HashMap<>();
 
     /**
      * The entry point of application.
@@ -21,10 +15,23 @@ public class PicoSpringboot {
         args = new String[]{"edu.escuelaing.arep.controller.AppController"};
         try{
             PicoSpringServer iocServer = PicoSpringServer.getInstance();
+            iocServer.setPort(getPort());
             iocServer.start(args);
         }catch (Exception ex){
             ex.printStackTrace();;
         }
+    }
+
+    /**
+     * Gets port.
+     *
+     * @return the port
+     */
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080;
     }
 
 
